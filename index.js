768816ff45462349894e0190d9f34b846f4fb85f
Marcio Coelho <marcio@caster.com.br>
2019-07-22 17:19:22 -0300

    Ajuste no controle de prioridades 13

--- index.js
+++ index.js
@@ -454,27 +454,42 @@ app.post('/atendimento/edita', verifyJWT, (req, res, next) => {
             if (parseInt(prior) < parseInt(antPrior)) {
               let sqlQry = "UPDATE pendencias SET PRIOR=PRIOR+1 WHERE PRIOR<" + parseInt(antPrior).toString() + 
                 " AND PRIOR>=" + parseInt(prior).toString() + " AND POSICAO<>'DISPONÍVEL' AND Id<>" + parseInt(atendId).toString();
-              comando.concat(sqlQry);
+                console.log('comando', comando);
+                console.log('sqlQry', sqlQry);
+                comando.concat(sqlQry);
+                console.log('comando+sqlQry', comando);
             }
             else if (parseInt(prior) > parseInt(antPrior)) {
               let sqlQry = "UPDATE pendencias SET PRIOR=PRIOR-1 WHERE PRIOR>1 AND PRIOR>" + parseInt(antPrior).toString() + 
                 " AND PRIOR<=" + parseInt(prior).toString() + " AND POSICAO<>'DISPONÍVEL' AND Id<>" + parseInt(atendId).toString();
+              console.log('comando', comando);
+              console.log('sqlQry', sqlQry);
               comando.concat(sqlQry);
+              console.log('comando+sqlQry', comando);
             }
           }
         } else {
           let sqlQry = "UPDATE pendencias SET PRIOR=PRIOR-1 WHERE PRIOR>" + parseInt(antPrior).toString() + " AND PRIOR > 1";
+          console.log('comando', comando);
+          console.log('sqlQry', sqlQry);
           comando.concat(sqlQry);
+          console.log('comando+sqlQry', comando);
         }
       }
       if (itemOK == 1){
         let sqlQry = "UPDATE pendencias SET PRIOR=PRIOR-1 WHERE PRIOR>" + parseInt(antPrior).toString() + " AND PRIOR > 1";
+        console.log('comando', comando);
+        console.log('sqlQry', sqlQry);
         comando.concat(sqlQry);
+        console.log('comando+sqlQry', comando);
       }
 
       let ultcomando = "update pendencias set cliente='" + cliente + "', prior=" + prior + ", tipo='" + tipoD + "', " + 
         "descricao='" + descricao + "', posicao='" + posicaoD + "', quemsolic='" + solic + "' where id=" + atendId.toString();
+      console.log('comando', comando);
+      console.log('ultcomando', ultcomando);
       comando.concat(ultcomando);
+      console.log('comando+ultcomando', comando);
       
   }).end();
 
